f9ce7025d4a449c95e06d2335ea254a007a7848b
Siddartraj333 <siddarthraaj.99@gmail.com>
2025-08-22 15:20:17 +0530

    email reminder added

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 // Health check
 app.get("/", (req, res) => res.send("server is running"));
@@ -46,6 +49,7 @@ app.post("/api/webhooks/clerk", async (req, res) => {
  * Inngest hits this to run your functions
  */
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
